63c564e88d314cb2926d2b4f630f621ebf2f04cc
parry <parry@parryc.com>
2025-03-09 20:34:09 -0500

    Add last commit timestamp

--- build.py
+++ build.py
@@ -1,6 +1,6 @@
 import codecs
 import os
-from format import get_html, md_to_html
+from format import md_to_html
 from jinja2 import Environment, PackageLoader, select_autoescape
 
 cwd = os.getcwd()
@@ -19,16 +19,6 @@ for page in os.scandir(pages):
         text = input_file.read()
         out_file = page.name.replace('.md', '')
         with open(os.path.join(dist, f'{out_file}.html'), 'w') as out:
-            data = {'html': md_to_html(text), 't': 'Georgian Verbs | ქართული ზმნები'}
+            data = {'html': md_to_html(text, page.path), 't': 'Georgian Verbs | ქართული ზმნები'}
             html = template.render(**data)
             print(html, file=out)
-
-
-# with open(filepath) as f:
-#     with open(dist, 'a') as out:
-#         data = yaml.safe_load(f)
-#         print(data)
-#         data['notes'] = md_to_html(data['notes'])
-#         template = env.get_template("designers.html")
-#         html = template.render(**data)
-#         print(html, file=out)
--- format.py
+++ format.py
@@ -1,40 +1,16 @@
-# from git import Repo
-# import os
+from git import Repo
+import os
 import markdown
-import codecs
 from bracket_table.bracket_table import BracketTable
 
 
-def get_html(filepath):
-    # repo = Repo(os.getcwd())
-    # if host == "zmnebi.com":
-    #     repo = Repo("/srv/data/vcs/git/default.git")
-    #     folder = os.path.join("templates", "zmnebi")
-    # else:
-    #     repo = Repo(os.getcwd())
-    #     folder = os.path.join(os.getcwd(), "templates", "zmnebi")
-    # filepath = os.path.join(current_app.root_path, "templates", page)
-    input_file = codecs.open(filepath, mode="r", encoding="utf-8")
-    text = input_file.read()
-    # time = repo.git.log("-n 1", "--format=%ci", "--", filepath)
-    # time = " ".join(time.split(" ")[0:2])
-    # text = (
-    #     "_Last updated {0}_\n".format(time) + "<a href='/rss'>RSS feed</a>\n\n" + text
-    # )
-    return markdown.markdown(
-        text,
-        extensions=[
-            "markdown.extensions.nl2br",
-            "markdown.extensions.toc",
-            "markdown.extensions.tables",
-            "markdown.extensions.def_list",
-            "markdown.extensions.abbr",
-            "markdown.extensions.footnotes",
-        ],
+def md_to_html(text, path):
+    repo = Repo(os.getcwd())
+    time = repo.git.log("-n 1", "--format=%ci", "--", path)
+    time = " ".join(time.split(" ")[0:2])
+    text = (
+        "_Last updated {0}_\n\n".format(time) + text
     )
-
-
-def md_to_html(text):
     return markdown.markdown(
         text,
         extensions=[
